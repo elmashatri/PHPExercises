ef22b0a7f831b60ac43da2c3e559d0725a89803f
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2023-04-23 21:22:14 +0200

    Add files via upload

---
+++ az.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    
+    // Write a PHP script to print letters from 'a' to 'z'.
+
+    for($x=ord('a'); $x<=ord('z'); $x++){
+        echo chr($x);
+        echo "<br>";
+    }
+
+    ?>
+
+    </body>
+</html>
