d73bb920534e348c455c780b84bf3b65600a22d6
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2023-04-23 20:28:46 +0200

    Add files via upload

---
+++ RemoveFirst.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    
+    /* 
+    
+    Write a PHP script to remove a part of a string from the beginning.
+    Sample string : 'rayy@example.com'
+    
+    */
+
+    $str='rayy@example.com';
+    echo substr($str,4);
+    echo "<br>";
+    
+    ?>
+
+    </body>
+</html>
