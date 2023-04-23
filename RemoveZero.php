b0489c910474feaed5daf6a23efea17672d71bc7
Elma Shatri <94225968+elmashatri@users.noreply.github.com>
2023-04-23 20:43:01 +0200

    Add files via upload

---
+++ RemoveZero.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <body>
+
+    <?php
+    
+    /* 
+    
+    Write a PHP script to remove all leading zeroes from a string.
+    Original String : '000547023.24'
+    
+    */
+
+    $x='000547023.24';
+    echo ltrim($x,'0');
+    echo "<br>";
+    
+    ?>
+
+    </body>
+</html>
